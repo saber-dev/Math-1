ef1d82fbb041af247bc915f65a1e6bb043c83ce2
nmahefa <nmahefa@gmail.com>
2015-08-15 20:36:31 +0200

    mise a jour du code, decrypt fonctionne

--- decrypt.php
+++ decrypt.php
@@ -14,30 +14,85 @@
 
   //fonction permettant de transformer le message codé en binaire
   function decryptMessToBin($key = array(), $mess = array()) {
-    $key_count     = count_tab($key);
-    $mess_count    = count_tab($mess);
-    $array_binarie = array();
-    $result        = null;
-    $bin_result    = null;
-    $tmp_array     = array();
+    $key_count      = count_tab($key);
+    $mess_count     = count_tab($mess);
+    $result         = null;
+    $bin_result     = null;
+    $full_bin_mess  = null;
+    $tmp_array      = array();
+    $array_new      = array();
+    $array_sort     = array();
+    $sort_bin_array = array();
+
+    // on met le tableau non ordonné dans un tableau vide
+    $array_new = $key;
+
+    natsort($array_new);
+    // on doit ordonner le tableau pour decrypter le message
+    foreach ($array_new as $values_in_array) {
+      array_push($array_sort, $values_in_array);
+    }
 
     // prendre les valeur dans le tableau
-    //boucle for en premier temps
     for ($i = 0; $i < $mess_count; $i++) {
       $tmp_value = $mess[$i];
       while ($tmp_value > 0) {
         for ($j = ($key_count -1); $j >= 0; $j--) {
-          if ($tmp_value >= $key[$j]) {
-            $tmp_value -= $key[$j];
-            $result    += $key[$j];
+          if ($tmp_value >= $array_sort[$j]) {
+            $tmp_value -= $array_sort[$j];
+            $result    += $array_sort[$j];
+
             // on push le resultat dans un tableau temporaire
-            array_push($tmp_array, $key[$j]);
+            array_push($tmp_array, $array_sort[$j]);
           }
         }
-        var_dump($tmp_array);
-        $tmp_array = array();
+
+        // la variable K doit etre égal au limiteur - 1, par exemple si on utilise 7 comme limiteur, alors k devra 6
+        for ($k = 6; $k >= 0; $k--) {
+          if (in_array($key[$k], $tmp_array)) {
+            $bin_result .= 1;
+          }
+          else {
+            $bin_result .= 0;
+          }
+        }
+        $full_bin_mess .= $bin_result;
+        $bin_result     = null;
+        $tmp_array      = array();
       }
     }
+    $sort_bin_array = sortBinMess($full_bin_mess);
+    sortBinMessToRealMess($sort_bin_array);
+  }
+
+  // ordonner le binaire en 8 bloc
+  function sortBinMess($mess) {
+    $tmp_mess      = null;
+    $bin_array     = array();
+    $length_mess   = strlen($mess);
+    $count_numbers = 1;
+
+    for ($i = 0; $i < $length_mess; $i++) {
+      $tmp_mess .= $mess[$i];
+      if ($count_numbers === 8) {
+        array_push($bin_array, $tmp_mess);
+        $tmp_mess = null;
+        $count_numbers = 0;
+      }
+      $count_numbers++;
+    }
+    // retourner un array ordonné en 8bloc cahcune
+    return $bin_array;
+  }
+
+  // la traduction du tableau de binaire et l'affichage du message
+  function sortBinMessToRealMess($tab = array()) {
+    $mess_decoded = null;
+
+    foreach ($tab as $result_values) {
+      $mess_decoded .= chr(bindec($result_values));
+    }
+    echo "message decodé : ", $mess_decoded, "\n";
   }
 
   // decoder la clé publique
@@ -51,24 +106,16 @@
       // ajout dans le tableau accompagné de son modulo
       array_push($tmp_array, my_modulo($tmp_result, $mod));
     }
-    // on tri le tableau pour former une suite superCroissante
-    natsort($tmp_array);
-    foreach ($tmp_array as $values_in_array) {
-      array_push($array_new, $values_in_array);
-    }
-    return $array_new;
+    return $tmp_array;
   }
 
   // fonction maitresse
-  function decryptCodeToBin($tab = array(),$d, $mod, $mess = array()) {
+  function decrypt($tab = array(),$d, $mod, $mess = array()) {
     $key_array     = array();
     $message_array = array();
     $key_array     = decryptKey($tab, $d, $mod);
-    if (super_croissance_check($key_array)) {
-      $message_array = decryptMess($mess, $mod, $d);
-      decryptMessToBin($key_array, $message_array);
-      // var_dump($message_array);
-    }
+    $message_array = decryptMess($mess, $mod, $d);
+    decryptMessToBin($key_array, $message_array);
   }
 
   //clé publique
@@ -76,4 +123,4 @@
   //message codé
   $tabmess = [1085, 515, 1185, 1128];
   // fonction de decodage
-  decryptCodeToBin($tabTmp, 47, 960, $tabmess);
+  decrypt($tabTmp, 47, 960, $tabmess);
